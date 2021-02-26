c7db50316da55c532880ab4e541703db5c9e718e
Cayal Walker <cayalw@gmail.com>
2021-02-26 16:55:25 -0500

    Adds hacky BPM updating

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,14 @@ const NOTE_RANGE : usize = 12;
 const CELL_DISPLAY_SIZE : f32 = 10.0;
 const CELL_DISPLAY_PADDING : f32 = 3.0;
 
+const BPM : f64 = 135.0;
+
 struct Model {
     _window: window::Id,
     cells: [[NoteCell; MEASURE_LENGTH]; NOTE_RANGE],
-    active_note: usize
+    active_note: usize,
+    cooldown: f64,
+    frames_since_tick: f64
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -21,14 +25,30 @@ fn main() {
         .run();
 }
 
-fn model(_app: &App) -> Model {
-    let _window = _app.new_window().view(view).build().unwrap();
+fn model(app: &App) -> Model {
+    let _window = app.new_window().view(view).build().unwrap();
     let blank_cells = [[NoteCell(false) ; MEASURE_LENGTH] ; NOTE_RANGE];
-    Model { _window, cells: blank_cells, active_note: 0}
+
+    let beats_per_second = BPM / 60.0;
+    let seconds_per_frame = 1.0 / 60.0; // TODO: Figure out why  app.fps(); is completely broken
+    let frames_per_beat = 1.0 / (beats_per_second * seconds_per_frame as f64);
+
+    Model {
+        _window,
+        cells: blank_cells,
+        active_note: 0,
+        cooldown: frames_per_beat,
+        frames_since_tick: 0.0
+    }
 }
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
+fn update(app: &App, model: &mut Model, _update: Update) {
+    if model.frames_since_tick < model.cooldown {
+        model.frames_since_tick += 1.0;
+        return;
+    }
     model.active_note = (model.active_note + 1) % 16;
+    model.frames_since_tick = 0.0;
 }
 
 fn view(_app: &App, model: &Model, _frame: Frame) {
